c29acc5cfefb3e599f32103759471ccea8e91fae
Srijan <chapssrijan@gmail.com>
2020-12-10 11:18:42 +0200

    Changes Allowed Hosts

--- testProject/settings.py
+++ testProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ear(+b*pji@1jo*)h=t8d3p&(3m+lvv*=1=hqhn&0*^-jg%4!+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','webshop-1-1.herokuapp.com']
 
